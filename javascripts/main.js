15636ab6c0f313f0c7c047088176ef699d2b3487
Josh Max <joshumax@hotmail.com>
2014-02-02 03:16:32 +0000

    Site Changes

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,42 +1,48 @@
-console.log('Hello from Mark and Josh');
-
-// EpicEditor -Josh
-var editor = new EpicEditor().load();
-
-var opts = {
-  container: 'epiceditor',
-  textarea: null,
-  basePath: 'epiceditor',
-  clientSideStorage: true,
-  localStorageName: 'epiceditor',
-  useNativeFullscreen: true,
-  parser: marked,
-  file: {
-    name: 'epiceditor',
-    defaultContent: '',
-    autoSave: 100
-  },
-  theme: {
-    base: '/themes/base/epiceditor.css',
-    preview: '/themes/preview/preview-dark.css',
-    editor: '/themes/editor/epic-dark.css'
-  },
-  button: {
-    preview: true,
-    fullscreen: true,
-    bar: "auto"
-  },
-  focusOnLoad: false,
-  shortcut: {
-    modifier: 18,
-    fullscreen: 70,
-    preview: 80
-  },
-  string: {
-    togglePreview: 'Toggle Preview Mode',
-    toggleEdit: 'Toggle Edit Mode',
-    toggleFullscreen: 'Enter Fullscreen'
-  },
-  autogrow: false
-}
-var editor = new EpicEditor(opts);
+// Hello from Mark and Josh
+$.ajax({
+    type: "GET",
+    url: "list.txt",
+    success: function(data) {
+        // EpicEditor -Josh
+        var opts = {
+        container: 'epiceditor',
+        textarea: null,
+        basePath: 'epiceditor',
+        clientSideStorage: false,
+        localStorageName: 'epiceditor',
+        useNativeFullscreen: true,
+        parser: marked,
+        file: {
+            name: 'epiceditor',
+            defaultContent: 'Loading...',
+            autoSave: 100
+        },
+        theme: {
+            base: '/themes/base/epiceditor.css',
+            preview: '/themes/preview/minimal.css',
+            editor: '/themes/editor/epic-dark.css'
+        },
+        button: false,
+        focusOnLoad: false,
+        shortcut: {
+            modifier: 18,
+            fullscreen: 70,
+            preview: 80
+        },
+        string: {
+            togglePreview: 'Toggle Preview',
+            toggleEdit: 'Toggle Edit',
+            toggleFullscreen: 'Enter Fullscreen'
+        },
+        autogrow: true
+        }
+        
+        var editor = new EpicEditor(opts).load();
+        editor.importFile('malicious-files', data);
+        editor.open('malicious-files');
+        editor.preview();
+    }, //END success fn
+    failure: function() {
+        console.log('failure');
+    }
+}); //END $.ajax
