011c2ffeb863656c49fcbe578b2b1d6c42215611
Josh Max <joshumax@hotmail.com>
2014-02-01 17:56:30 -0800

    Basic Changes

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,42 @@
-console.log('This would be the main JS file.');
+console.log('Hello from Mark and Josh');
+
+// EpicEditor -Josh
+var editor = new EpicEditor().load();
+
+var opts = {
+  container: 'epiceditor',
+  textarea: null,
+  basePath: 'epiceditor',
+  clientSideStorage: true,
+  localStorageName: 'epiceditor',
+  useNativeFullscreen: true,
+  parser: marked,
+  file: {
+    name: 'epiceditor',
+    defaultContent: '',
+    autoSave: 100
+  },
+  theme: {
+    base: '/themes/base/epiceditor.css',
+    preview: '/themes/preview/preview-dark.css',
+    editor: '/themes/editor/epic-dark.css'
+  },
+  button: {
+    preview: true,
+    fullscreen: true,
+    bar: "auto"
+  },
+  focusOnLoad: false,
+  shortcut: {
+    modifier: 18,
+    fullscreen: 70,
+    preview: 80
+  },
+  string: {
+    togglePreview: 'Toggle Preview Mode',
+    toggleEdit: 'Toggle Edit Mode',
+    toggleFullscreen: 'Enter Fullscreen'
+  },
+  autogrow: false
+}
+var editor = new EpicEditor(opts);
